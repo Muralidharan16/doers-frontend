238bcbbf1b46aaae71e910c3a8de3f76f80a888a
Muralidharan16 <muralirajan3333@gmail.com>
2026-06-04 14:18:56 +0530

    chore: add branch contacts and onboarding type definitions

--- src/hooks/useFocalPoint.d.ts
+++ src/hooks/useFocalPoint.d.ts
@@ -1,5 +1,5 @@
-import { RefObject } from 'react';
-export declare function useFocalPoint(containerRef: RefObject<HTMLDivElement>): {
+import { type RefObject } from 'react';
+export declare function useFocalPoint(containerRef: RefObject<HTMLDivElement | null>): {
     focalY: number;
     setFocalY: import("react").Dispatch<import("react").SetStateAction<number>>;
     handleMouseDown: (e: React.MouseEvent) => void;
